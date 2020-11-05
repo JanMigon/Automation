746fb363a13514243dd97b8b6c8dc82d72fcb621
Jan Migoń <jan.migon@consonance.tech>
2020-11-06 00:12:53 +0100

    Automate laboratory registration

---
+++ automatic_web_browsing/configuration/config.py
@@ -0,0 +1,2 @@
+config={'url': 'https://upel2.cel.agh.edu.pl/weaiib/', 'username': 'xxx', 'password': 'xxx',
+        'course': '', 'link_of_interest': "//div[@class='activityinstance']/a[@href='https://upel2.cel.agh.edu.pl/weaiib/mod/attendance/view.php?id=19391']"}
